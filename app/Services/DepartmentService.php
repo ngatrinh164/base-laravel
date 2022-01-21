940cfbd78338d9dd22a6030ff2d3bb55d6b58782
ngatrinh <ttntrinhnga@gmail.com>
2022-01-21 20:26:13 +0700

    refactor: add service

---
+++ app/Services/DepartmentService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DepartmentService
+{
+    use HasFactory;
+}
