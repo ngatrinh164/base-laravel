ca8410870bf5402966a235dab91fefd2353fbcd8
ngatrinh <ttntrinhnga@gmail.com>
2022-01-22 09:32:52 +0700

    feat: add fake db to test

---
+++ qltb_fake_db/kltn_api_equipment_status.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
+--
+-- Host: 127.0.0.1    Database: kltn_api
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `equipment_status`
+--
+
+DROP TABLE IF EXISTS `equipment_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `equipment_status` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `equipment_id` int NOT NULL,
+  `type` int NOT NULL,
+  `status` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipment_status`
+--
+
+LOCK TABLES `equipment_status` WRITE;
+/*!40000 ALTER TABLE `equipment_status` DISABLE KEYS */;
+INSERT INTO `equipment_status` VALUES (1,7,2,3,NULL,NULL),(2,3,8,4,NULL,NULL),(3,4,5,3,NULL,NULL),(4,4,3,3,NULL,NULL),(5,4,2,4,NULL,NULL),(6,8,2,2,NULL,NULL),(7,1,7,2,NULL,NULL),(8,9,4,1,NULL,NULL),(9,2,7,5,NULL,NULL),(10,1,10,1,NULL,NULL),(11,1,6,5,NULL,NULL),(12,9,5,1,NULL,NULL),(13,2,9,1,NULL,NULL),(14,5,1,2,NULL,NULL),(15,5,7,2,NULL,NULL),(16,7,6,3,NULL,NULL),(17,3,5,5,NULL,NULL),(18,6,4,5,NULL,NULL),(19,3,1,4,NULL,NULL);
+/*!40000 ALTER TABLE `equipment_status` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-22  9:31:13
